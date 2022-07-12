708109a759d4d9c921ba371c96e7476b23ddc510
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2022-07-12 13:57:47 -0400

    Routerlink
    added router links for the 2 components

--- Inventory/src/app/app-routing.module.ts
+++ Inventory/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewMerchandiseComponent } from './new-merchandise/new-merchandise.component';
+import { MerchandiseListComponent } from './merchandise-list/merchandise-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'add', component: NewMerchandiseComponent
+  }, 
+  {
+    path: 'list', component: MerchandiseListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
